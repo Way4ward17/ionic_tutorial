c806bca3299889afdab1055797b59be149316c32
way4ward17 <supersondultimate@gmail.com>
2020-09-13 23:03:21 +0100

    editing button

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const reason = document.querySelector("#input-reason");
+const amount = document.querySelector("#input-amount");
+const clear = document.querySelector("#btn-clear");
+const add = document.querySelector("#btn-add");
+
+
+
+clear.addEventListener('click', ()=>{
+    console.log("clicked");
+})
+
